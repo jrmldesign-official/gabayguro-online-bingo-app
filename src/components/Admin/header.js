9994aa2b8b7b218e77d4c2a50233e1835484897a
jrmldesign-official <jrmldesign-official@support.github.com>
2020-09-24 16:37:17 +0800

    ADD CORS

--- src/components/Admin/header.js
+++ src/components/Admin/header.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-var api = axios.create({
-    baseURL: 'https://binggo-test.dokyumento.asia/index.php/',
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-    },
-});
 
 class header extends Component {
 
@@ -23,7 +16,7 @@ class header extends Component {
     }
 
     getUserDetails = async () => {
-        api.post(`Useraccounts/fetch_user_details_by_user_id`, { user_id: localStorage.user_id })
+        axios.post(`Useraccounts/fetch_user_details_by_user_id`, { user_id: localStorage.user_id })
         .then(res => {
 
             localStorage.setItem("name", res.data.payload[0].user_fullname)
